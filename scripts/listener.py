8f74025de19cc97b9f12ff8b6592e5068abec7d2
radamsfr <radams.official1@gmail.com>
2024-11-17 10:43:05 -0800

    add scripts folder

---
+++ scripts/listener.py
@@ -0,0 +1,22 @@
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+
+f = open("timestamps.pkl", "w")
+
+def callback(data):
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.header.stamp)
+
+    a = data.header.stamp.secs
+    print(a)
+    f.write(str(a) + '\n')
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('/docking_control/vision_pose/pose', PoseStamped, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    print("listening...")
+    listener()
