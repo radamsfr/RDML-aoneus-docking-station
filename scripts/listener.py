13f1307a8ed6cdacd596e8f671cbcb8bcb4b38fb
radamsfr <radams.official1@gmail.com>
2024-11-21 22:23:35 +0000

    write TODO

--- scripts/listener.py
+++ scripts/listener.py
@@ -1,17 +1,34 @@
 import rospy
+import numpy
+from scipy.spatial.transform import Rotation as R
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image
 from marine_acoustic_msgs.msg import ProjectedSonarImage
 
-f = open("timestamps.pkl", "w")
+cameras_sphere = {}
+
+
+f = open("camera_pose.txt", "w")
 
 def callback(data):
     # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.header.stamp)
 
-    a = data.header.stamp.secs
-    print(a)
-    f.write(str(a) + '\n')
+    position = data.pose.position
+    quaternion = data.pose.orientation
+    
+    # TODO make translation matrix
+    # collect position and quaternion values
+    # convert quaternion values to rotation matrix
+    # combine to make translation matrix
+    
+    # TODO
+    # store in npz file with camera_matrix, scale_matrix, and inv_matrices
+    
+    
+    # print("position:\n" + str(position) + '\n')
+    print(f"rotation:\n{str(rotation)}\n")
+    f.write(str(rotation) + '\n\n')
 
 def listener():
     rospy.init_node('listener', anonymous=True)
